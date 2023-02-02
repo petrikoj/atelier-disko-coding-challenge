ce4fa093c925903a5f2d7493829ef5857cea4fac
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-02-02 15:52:17 +0100

    Reworked textarea component to be reusable

--- components/textarea/textarea.tsx
+++ components/textarea/textarea.tsx
@@ -1,38 +1,9 @@
-import { type } from "os";
+import React from "react";
 import styles from "./textarea.module.css";
-import { useState } from "react";
 
-type TextAreaInput = {
-  input: string;
-};
-
-function TextArea({ input }: TextAreaInput) {
-  const [messageText, setMessageText] = useState("");
-  const handleMessageHandler = (event) => {
-    setMessageText(event.target.value);
-  };
-
-  return (
-    <div>
-      <textarea
-        className={styles.root}
-        name="message"
-        id="message"
-        placeholder="Anything else we should know?"
-        maxLength="100"
-        onChange={handleMessageHandler}
-      >
-        {input}
-      </textarea>
-      <div
-        className={
-          messageText.length < 80 ? styles.counter : styles.counterWarning
-        }
-        id="counter"
-      >
-        {100 - messageText.length} characters left
-      </div>
-    </div>
-  );
+function TextArea({
+  ...rest
+}: React.TextareaHTMLAttributes<HTMLTextAreaElement>) {
+  return <textarea className={styles.root} {...rest} />;
 }
 export default TextArea;
