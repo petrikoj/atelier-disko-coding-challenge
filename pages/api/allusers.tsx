7b504e2aff666313783aab8e106f56987e42279c
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-02-03 19:26:00 +0100

    WIP sorting results

--- pages/api/allusers.tsx
+++ pages/api/allusers.tsx
@@ -3,9 +3,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+      orderBy: { firstname: "desc" }
+    });
     res.status(200).json({ users });
-    console.log("ALL USERS --->", users);
   } catch (error) {
     console.log(error);
     res.status(403).json({ error: "Error while fetching all users" });
