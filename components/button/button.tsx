c4444c75d9d9e0dbc2a24aa0262cc90d5e45bd0d
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-02-06 19:12:57 +0100

    Minor adjustments

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,15 +1,15 @@
 import styles from "./button.module.css";
 
-type ButtonContentProps = {
+type ButtonProps = {
   children?: React.ReactElement | React.ReactElement[] | string;
-};
-type ButtonProps = ButtonContentProps &
-  React.ButtonHTMLAttributes<HTMLButtonElement>;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button({ children, ...rest }: ButtonProps) {
   return (
     <div className={styles.root}>
-      <button className={styles.button}>{children}</button>
+      <button className={styles.button} {...rest}>
+        {children}
+      </button>
     </div>
   );
 }
