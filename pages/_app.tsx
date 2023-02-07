183dc3ed744330d800c3e6f6169536217534af17
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-02-07 14:25:26 +0100

    Formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/reset.css";
 import "../styles/variables.css";
 import "../styles/globals.css";
-
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
