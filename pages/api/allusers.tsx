6075651de93b03fd7cdbb317ba3fabd72e85d536
petrikoj <100421329+petrikoj@users.noreply.github.com>
2023-02-07 14:24:30 +0100

    DONE: API authentication process (todo: err page)

--- pages/api/allusers.tsx
+++ pages/api/allusers.tsx
@@ -2,13 +2,22 @@ import { prisma } from "../../db";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const users = await prisma.user.findMany({
-      orderBy: { firstname: "desc" },
-    });
-    res.status(200).json({ users });
-  } catch (error) {
-    console.log(error);
-    res.status(403).json({ error: "Error while fetching all users" });
+  if (req.headers.authorization !== `Bearer ${process.env.API_TOKEN}`) {
+    return res
+      .status(401)
+      .json({ status: 401, message: "Unauthorized request" });
+  }
+  if (req.headers.authorization === `Bearer ${process.env.API_TOKEN}`) {
+    try {
+      const users = await prisma.user.findMany({
+        orderBy: { firstname: "desc" },
+      });
+      res.status(200).json({ users });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(403)
+        .json({ status: 403, message: "Error while fetching all users" });
+    }
   }
 };
--- pages/registration/all.tsx
+++ pages/registration/all.tsx
@@ -1,24 +1,28 @@
 import { User } from "@prisma/client";
 import got from "got";
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps, NextApiResponse } from "next";
 import Table, {
   ColumnDefinitionType,
 } from "../../components/dataTable/dataTable";
 import Header from "../../components/header/header";
 
-export interface ApiResponse {
+export type ApiResponse = {
   data: {
     users: User[];
   };
-}
-// Fetching the data with getServerSideProps and pass it to the
-// page component as props
+} & NextApiResponse;
+
+// Fetching the data as an authorized request with getServerSideProps
+// and pass it to the page component as props
 export const getServerSideProps: GetServerSideProps = async () => {
-  const url = `${process.env.NEXT_PUBLIC_SITE_HOST}`;
-  console.log(url);
-  const data: ApiResponse = await got(url + "/api/allusers").json();
+  const url = `${process.env.NEXT_PUBLIC_SITE_HOST}/api/allusers`;
+  const options = {
+    headers: { Authorization: `Bearer ${process.env.API_TOKEN}` },
+  };
+  const data: ApiResponse = await got(url, options).json();
   return { props: { data } };
 };
+
 // Rendering the data in the generic table component by defining
 // the columns based on the properties of the User type
 const Users = ({ data }: ApiResponse) => {
